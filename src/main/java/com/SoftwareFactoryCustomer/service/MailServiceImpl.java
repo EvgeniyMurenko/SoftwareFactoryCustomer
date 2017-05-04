6c9863811ac8bbd7876b92550e362d591a6a19b0
EvgeniyMurenko <mr_jeka@bk.ru>
2017-05-04 14:38:51 +0300

    mail service

--- src/main/java/com/SoftwareFactoryCustomer/service/MailServiceImpl.java
+++ src/main/java/com/SoftwareFactoryCustomer/service/MailServiceImpl.java
@@ -17,6 +17,83 @@ public class MailServiceImpl implements MailService {
     @Autowired
     JavaMailSender mailSender;
 
+    private String mailHeader = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n" +
+            "        \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
+            "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+            "<head>\n" +
+            "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>\n" +
+            "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"/>\n" +
+            "    <meta name=\"viewport\"\n" +
+            "          content=\"width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, minimal-ui\"/>\n" +
+            "    <meta name=\"format-detection\" content=\"telephone=no\"/>\n" +
+            "    <meta name=\"format-detection\" content=\"address=no\"/>\n" +
+            "\n" +
+            "    <title>소프트웨어팩토리</title>\n" +
+            "\n" +
+            "    <style type=\"text/css\">\n" +
+            "        @import url(http://fonts.googleapis.com/earlyaccess/nanumgothic.css);\n" +
+            "\n" +
+            "        html, body {\n" +
+            "            font-family: 'Nanum Gothic', Arial, Tahoma, Verdana, sans-serif;\n" +
+            "            font-size: 11pt;\n" +
+            "            width: 100%;\n" +
+            "        }\n" +
+            "    </style>\n" +
+            "\n" +
+            "</head>\n" +
+            "<body style=\"margin: 0; padding: 0; -webkit-font-smoothing: antialiased;\n" +
+            "            mso-margin-top-alt: 0; mso-margin-bottom-alt: 0; mso-padding-alt: 0; background: #E7E7E7;\">\n" +
+            "<div style=\"width: 850px; height: 100%;\n" +
+            "            background: #ffffff; overflow: hidden; margin: 20px auto 40px auto; box-shadow: 0 0 7px #cdcccc;\n" +
+            "            -webkit-box-shadow: 0 0 7px #cdcccc; -moz-box-shadow: 0 0 7px #cdcccc; -o-box-shadow: 0 0 7px #cdcccc;\">\n" +
+            "\n" +
+            "    <!-- Header -->\n" +
+            "    <div style=\"padding: 10px 0 15px 0; margin: 10px; border-bottom: 1px solid #eee; height: 55px;\">\n" +
+            "        <div style=\"float: left;\">\n" +
+            "            <a href=\"http://www.sofac.kr\" target=\"_blank\" style=\"text-decoration: none; font-size: 24pt; color: #333;\">소프트웨어<span\n" +
+            "                        style=\"font-weight: 600; color: #03a9f4;\">팩토리</span></a>\n" +
+            "            <div style=\"font-size: 9pt; color: #999;\">SoFAC : <span style=\"font-style: italic;\">Software Factory</span>\n" +
+            "            </div>\n" +
+            "        </div>\n" +
+            "\n" +
+            "        <div style=\"float: right; padding-top: 15px;\">\n" +
+            "            <a href=\"http://www.sofac.kr/whatIsSofac\" target=\"_blank\" style=\"color: #03a9f4; cursor: pointer !important; text-decoration: none;\">소프트웨어 팩토리 란 무엇입니까?</a>\n" +
+            "        </div>\n" +
+            "    </div>\n" +
+            "    <!-- #End Header -->\n" +
+            "\n" +
+            "    <!-- Content -->\n" +
+            "    <div style=\"margin: 20px 10px; content: ''; clear: both;\">\n" +
+            "\n";
+
+    private String mailFooter =  "        <p>No need to reply to this email.</p>\n" +
+            "\n" +
+            "    </div>\n" +
+            "    <!-- #End Content -->\n" +
+            "\n" +
+            "    <!-- Footer -->\n" +
+            "    <div style=\"height: 50px; margin: 20px 10px 10px 10px; padding-top: 15px; border-top: 1px solid #eee;\">\n" +
+            "\n" +
+            "        <div style=\"color: #999; font-size: 9pt; float: left;\">\n" +
+            "            Policy of SoFAC<br/>\n" +
+            "            <a href=\"http://www.sofac.kr/policy\" style=\"color: #03a9f4; cursor: pointer !important; text-decoration: none;\"><span style=\"font-size: 9pt;\">SoFAC 고객 정책</span></a>\n" +
+            "        </div>\n" +
+            "        <div style=\"color: #999; font-size: 9pt; float: right; text-align: right;\">\n" +
+            "            220-87-45112<br/>\n" +
+            "            서울시 강남구 역삼동 해성빌딩 7층<br/>\n" +
+            "            대표자 : 박상만\n" +
+            "        </div>\n" +
+            "\n" +
+            "    </div>\n" +
+            "    <div style=\"color: #999; font-size: 9pt; text-align: center; padding: 10px;\">\n" +
+            "        Copyright © 2016. All rights reserved\n" +
+            "    </div>\n" +
+            "    <!-- Footer -->\n" +
+            "\n" +
+            "</div>\n" +
+            "</body>\n" +
+            "</html>";
+
 
     @Override
     public void sendEmailAfterRegistration(String password, String login , String recipientMail , String recipientName) {
@@ -29,6 +106,7 @@ public class MailServiceImpl implements MailService {
                             new InternetAddress(recipientMail));
                     mimeMessage.setSubject("소프트웨어팩토리에 가입 하신 것을 축하드립니다." , "utf-8");
                     mimeMessage.setContent(
+                            mailHeader+"\n"+
                             "안녕하세요 "+ recipientName + " 고객님" +
                                     "<br>" +
                                     "먼저 소프트웨어팩토리에 회원이 되신 것을 감사드립니다." +
@@ -59,7 +137,8 @@ public class MailServiceImpl implements MailService {
                                     "소프트웨어팩토리는 고객님이 필요로 하는 다양한 종류의 소프트웨어 개발 대행 및 유지보수 대행을 전문적으로 하는 다국적 기업입니다.</p>\n" +
                                     "\n" +
                                     "<br>\n" +
-                                    "<p>(이 메일은 발송 전용 메일 입니다.)</i></p>", "text/html; charset=utf-8" );
+                                    "<p>(이 메일은 발송 전용 메일 입니다.)</i></p>"+
+                            "\n"+mailFooter, "text/html; charset=utf-8" );
 
                 }
             });
@@ -81,7 +160,7 @@ public class MailServiceImpl implements MailService {
                             new InternetAddress(recipientMail));
                     mimeMessage.setSubject("[자동발송] 소프트웨어팩토리에 문의해 주셔서 감사드립니다. (" + estimateId + ")", "utf-8");
                     mimeMessage.setContent(
-                            "<p><b>접수번호 : " + estimateId + "</b></p>" +
+                            mailHeader+"\n"+"<p><b>접수번호 : " + estimateId + "</b></p>" +
                                     "<p>먼저 소프트웨어팩토리에 문의해 주셔서 감사드립니다.<br>\n" +
                                     "문의 하신 내용은 대부분 소프트웨어를 만들기 위한 아이디어나 정해진 작업 내용에 대한 가격 및 업무 방법일 것입니다.</p>\n" +
                                     "<p>따라서 고객님의 요청내용은 작업의 종류의 규모를 기준으로 기술적인 견적과 업무 방법 그리고 기초적인 분석 자료를 함께 보내드립니다.</p>\n" +
@@ -102,7 +181,7 @@ public class MailServiceImpl implements MailService {
                                     "\n" +
                                     "<p>감사합니다.</p>\n" +
                                     "\n" +
-                                    "<p><i>(이 메일은 발송 전용 메일 입니다.)</i></p>", "text/html; charset=utf-8");
+                                    "<p><i>(이 메일은 발송 전용 메일 입니다.)</i></p>"+"\n"+mailFooter, "text/html; charset=utf-8");
 
                 }
             });
@@ -120,83 +199,9 @@ public class MailServiceImpl implements MailService {
                    mimeMessage.setRecipient(Message.RecipientType.TO,
                            new InternetAddress(recipientMail));
                    mimeMessage.setSubject("You have a new message in sofac.kr", "utf-8");
-                   mimeMessage.setContent("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n" +
-                           "        \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
-                           "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
-                           "<head>\n" +
-                           "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>\n" +
-                           "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"/>\n" +
-                           "    <meta name=\"viewport\"\n" +
-                           "          content=\"width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, minimal-ui\"/>\n" +
-                           "    <meta name=\"format-detection\" content=\"telephone=no\"/>\n" +
-                           "    <meta name=\"format-detection\" content=\"address=no\"/>\n" +
-                           "\n" +
-                           "    <title>소프트웨어팩토리</title>\n" +
-                           "\n" +
-                           "    <style type=\"text/css\">\n" +
-                           "        @import url(http://fonts.googleapis.com/earlyaccess/nanumgothic.css);\n" +
-                           "\n" +
-                           "        html, body {\n" +
-                           "            font-family: 'Nanum Gothic', Arial, Tahoma, Verdana, sans-serif;\n" +
-                           "            font-size: 11pt;\n" +
-                           "            width: 100%;\n" +
-                           "        }\n" +
-                           "    </style>\n" +
-                           "\n" +
-                           "</head>\n" +
-                           "<body style=\"margin: 0; padding: 0; -webkit-font-smoothing: antialiased;\n" +
-                           "            mso-margin-top-alt: 0; mso-margin-bottom-alt: 0; mso-padding-alt: 0; background: #E7E7E7;\">\n" +
-                           "<div style=\"width: 850px; height: 100%;\n" +
-                           "            background: #ffffff; overflow: hidden; margin: 20px auto 40px auto; box-shadow: 0 0 7px #cdcccc;\n" +
-                           "            -webkit-box-shadow: 0 0 7px #cdcccc; -moz-box-shadow: 0 0 7px #cdcccc; -o-box-shadow: 0 0 7px #cdcccc;\">\n" +
-                           "\n" +
-                           "    <!-- Header -->\n" +
-                           "    <div style=\"padding: 10px 0 15px 0; margin: 10px; border-bottom: 1px solid #eee; height: 55px;\">\n" +
-                           "        <div style=\"float: left;\">\n" +
-                           "            <a href=\"http://www.sofac.kr\" target=\"_blank\" style=\"text-decoration: none; font-size: 24pt; color: #333;\">소프트웨어<span\n" +
-                           "                        style=\"font-weight: 600; color: #03a9f4;\">팩토리</span></a>\n" +
-                           "            <div style=\"font-size: 9pt; color: #999;\">SoFAC : <span style=\"font-style: italic;\">Software Factory</span>\n" +
-                           "            </div>\n" +
-                           "        </div>\n" +
-                           "\n" +
-                           "        <div style=\"float: right; padding-top: 15px;\">\n" +
-                           "            <a href=\"http://www.sofac.kr/whatIsSofac\" target=\"_blank\" style=\"color: #03a9f4; cursor: pointer !important; text-decoration: none;\">소프트웨어 팩토리 란 무엇입니까?</a>\n" +
-                           "        </div>\n" +
-                           "    </div>\n" +
-                           "    <!-- #End Header -->\n" +
-                           "\n" +
-                           "    <!-- Content -->\n" +
-                           "    <div style=\"margin: 20px 10px; content: ''; clear: both;\">\n" +
-                           "\n" +
-
+                   mimeMessage.setContent(mailHeader+"\n"+
                            "        <p>"+respondText+"</p>\n" +
-                           "        <p>No need to reply to this email.</p>\n" +
-                           "\n" +
-                           "    </div>\n" +
-                           "    <!-- #End Content -->\n" +
-                           "\n" +
-                           "    <!-- Footer -->\n" +
-                           "    <div style=\"height: 50px; margin: 20px 10px 10px 10px; padding-top: 15px; border-top: 1px solid #eee;\">\n" +
-                           "\n" +
-                           "        <div style=\"color: #999; font-size: 9pt; float: left;\">\n" +
-                           "            Policy of SoFAC<br/>\n" +
-                           "            <a href=\"http://www.sofac.kr/policy\" style=\"color: #03a9f4; cursor: pointer !important; text-decoration: none;\"><span style=\"font-size: 9pt;\">SoFAC 고객 정책</span></a>\n" +
-                           "        </div>\n" +
-                           "        <div style=\"color: #999; font-size: 9pt; float: right; text-align: right;\">\n" +
-                           "            220-87-45112<br/>\n" +
-                           "            서울시 강남구 역삼동 해성빌딩 7층<br/>\n" +
-                           "            대표자 : 박상만\n" +
-                           "        </div>\n" +
-                           "\n" +
-                           "    </div>\n" +
-                           "    <div style=\"color: #999; font-size: 9pt; text-align: center; padding: 10px;\">\n" +
-                           "        Copyright © 2016. All rights reserved\n" +
-                           "    </div>\n" +
-                           "    <!-- Footer -->\n" +
-                           "\n" +
-                           "</div>\n" +
-                           "</body>\n" +
-                           "</html>", "text/html; charset=utf-8");
+                          mailFooter, "text/html; charset=utf-8");
 
                }
            });
